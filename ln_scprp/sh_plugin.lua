18c0704607179d0723bfb51876e241ddfeb1d847
KarmaLN <chamelphatt@yahoo.de>
2021-02-14 23:14:49 +0100

    Add files via upload

---
+++ ln_scprp/sh_plugin.lua
@@ -0,0 +1,12 @@
+
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "[LN] SCPRP"
+PLUGIN.author = "Karma"
+PLUGIN.description = "Adds Several Options"
+
+ix.util.Include("sh_rules.lua")
+ix.util.Include("cl_rules.lua")
+ix.util.Include("sh_teamchat.lua")
+
+

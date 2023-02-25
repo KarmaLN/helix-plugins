24b3ef58b9213e17f90c027a6fa2fe470fdfe3d0
KarmaLN <chamelphatt28@protonmail.com>
2023-02-25 12:11:34 +0200

    Update flashlight_antispam.lua

--- flashlight_antispam.lua
+++ flashlight_antispam.lua
@@ -10,13 +10,13 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
 
-PLUGIN.cooldown = 4
+local cooldown = 4
 
 function PLUGIN:PlayerSwitchFlashlight(client, enabled)
 	if (client.ixFlashlightEnable or 0) > CurTime() and enabled then
 		return false
 	else
-		client.ixFlashlightEnable = CurTime() + PLUGIN.cooldown
+		client.ixFlashlightEnable = CurTime() + cooldown
 		return true
 	end
 end
